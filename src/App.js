d415738b53d17e4ced9bdaa9c56475b6184b71fc
changwooj <wjdckddn99@naver.com>
2025-10-23 09:52:59 +0900

    feat(console): 입력 기능 추가
    - 자동자 이름 문자열로 입력 기능 추가
    - 시도할 이동 횟수 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import { getCarNamesInput, getRoundInput } from "./view/console";
+
 class App {
-  async run() {}
+  async run() {
+    const carNamesInput = await getCarNamesInput();
+    const roundInput = await getRoundInput();
+  }
 }
 
 export default App;
